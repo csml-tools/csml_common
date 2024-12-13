b167adbfb6bb84ebf2507747f28b0f0ee31079f3
Evgenii Rechkalov <snorkysnark@gmail.com>
2024-12-13 20:00:16 +0100

    rename yaml loader function

--- csml_common/cli_args.py
+++ csml_common/cli_args.py
@@ -4,7 +4,7 @@ from pydantic import BaseModel
 import yaml
 
 
-def pydantic_yaml_loader[T: BaseModel](type_: type[T]) -> Callable[[str], T]:
+def create_yaml_loader[T: BaseModel](type_: type[T]) -> Callable[[str], T]:
     def parser(path: str):
         with Path(path).open() as file:
             return type_.model_validate(yaml.safe_load(file))
@@ -12,4 +12,4 @@ def pydantic_yaml_loader[T: BaseModel](type_: type[T]) -> Callable[[str], T]:
     return parser
 
 
-__all__ = ["pydantic_yaml_loader"]
+__all__ = ["create_yaml_loader"]
