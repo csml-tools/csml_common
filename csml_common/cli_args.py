cc82386838c40cc5d3b995a2d61c43959fc253cd
Evgenii Rechkalov <snorkysnark@gmail.com>
2024-12-23 15:52:56 +0100

    back to click ParamTypes

--- csml_common/cli_args.py
+++ csml_common/cli_args.py
@@ -1,27 +1,59 @@
+import click
 from pathlib import Path
-from typing import Callable
+from typing import Any, Optional
 from pydantic import BaseModel
 import yaml
 
 
-def create_yaml_loader[T: BaseModel](type_: type[T]) -> Callable[[str], T]:
-    def parser(path: str):
-        with Path(path).open() as file:
-            return type_.model_validate(yaml.safe_load(file))
+class YamlParamType(click.ParamType):
+    def __init__(self, type_: type[BaseModel]) -> None:
+        self.__type = type_
+        self.name = type_.__name__
 
-    return parser
+    def __load(self, path: Path):
+        with path.open() as file:
+            return self.__type.model_validate(yaml.safe_load(file))
+
+    def convert(
+        self, value: Any, param: Optional[click.Parameter], ctx: Optional[click.Context]
+    ) -> Any:
+        if isinstance(value, self.__type):
+            return value
+        elif isinstance(value, str):
+            return self.__load(Path(value))
+        elif isinstance(value, Path):
+            return self.__load(value)
+        else:
+            self.fail(f"Unexpected type: {type(value)}", param, ctx)
 
 
-def range_parser(value: str) -> range:
-    parts = value.split("-")
+class RangeParamType(click.ParamType):
+    name = "range"
 
-    if len(parts) == 2:
-        return range(int(parts[0]), int(parts[1]) + 1)
-    elif len(parts) == 1:
-        num = int(parts[0])
-        return range(num, num + 1)
-    else:
-        raise ValueError(f"{value} is not a range string expected format like '0-100'")
+    def convert(
+        self, value: Any, param: Optional[click.Parameter], ctx: Optional[click.Context]
+    ) -> Any:
+        if isinstance(value, range):
+            return range
+        elif isinstance(value, str):
+            parts = value.split("-")
+
+            if len(parts) == 2:
+                return range(int(parts[0]), int(parts[1]) + 1)
+            elif len(parts) == 1:
+                num = int(parts[0])
+                return range(num, num + 1)
+            else:
+                self.fail(
+                    f"{value} is not a range string expected format like '0-100'",
+                    param,
+                    ctx,
+                )
+        else:
+            self.fail("Must be str or range", param, ctx)
 
 
-__all__ = ["create_yaml_loader", "range_parser"]
+range_param_type = RangeParamType()
+
+
+__all__ = ["YamlParamType", "range_param_type"]
